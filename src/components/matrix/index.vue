1ca533f81843aaad7c1042848f23c01d8923cd5e
zelin.xu <zelin.xu@shopee.com>
2022-04-14 20:46:29 +0800

    feat: loader test

---
+++ src/components/matrix/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="matrix-change"></div>
+</template>
+
+<script lang="ts">
+export default {
+  mounted() {
+    import ('./index');
+  },
+}
+</script>
+
+
+<style>
+
+</style>
